fdd0836fb9606331d541fb95b074a6bbb7953649
cyschneck <22159116+cyschneck@users.noreply.github.com>
2020-12-24 01:24:36 -0700

    global label for terrian types

--- image_class.py
+++ image_class.py
@@ -4,16 +4,19 @@
 # Date Started: December 2020
 ########################################################################################################################################
 from datetime import datetime
+import os
 
-def preProcessing(image_file, to_print):
+labels = ["craters", "plains", "labyrinth", "hummocky", "lakes", "dunes"] # terrian types
+
+def preProcessing(image_directory, to_print):
 	# run pre-processing on image file
-	if to_print: print("\ngiven image file: {0}".format(image_file))
+	if to_print: print("\ntotal image files in '{0}' = {1}".format(image_directory, len(os.listdir(image_directory))))
 
 #### IMAGE TERRAIN CLASSIFERS: MORPHOLOGICAL and ALBEDO
-### TYPES: Craters, Plains, Labyrinth, Hummocky Terrain, Lakes, Dunes
+#### TYPES: Craters, Plains, Labyrinth, Hummocky Terrain, Lakes, Dunes
 
-## MORPHOLOGICAL
-def morphologicalClassifer():
+## MORPHOLOGICAL CLASSIFER
+def morphologicalClassifer(image_directory, to_print):
 	###############################################
 	## Based on the physical features
 	#
@@ -26,10 +29,11 @@ def morphologicalClassifer():
 	# Dunes: Long, narrow features with regular spacing
 	#
 	###############################################
-	pass
+	if to_print: print("\nMorphological Classifer")
+	if to_print: print("total image files in '{0}' = {1}".format(image_directory, len(os.listdir(image_directory))))
 
-## ALBEDO
-def albedoClassifer():
+## ALBEDO CLASSIER
+def albedoClassifer(image_directory, to_print):
 	###############################################
 	## Based on the light or darkness of a region
 	#
@@ -42,7 +46,8 @@ def albedoClassifer():
 	# Dunes: Dark in SAR, High Emissivity in RADAR
 	#
 	###############################################
-	pass
+	if to_print: print("\nAlbedo Classifer")
+	if to_print: print("total image files in '{0}' = {1}".format(image_directory, len(os.listdir(image_directory))))
 
 if __name__ == '__main__':
 	start_time = datetime.now()
@@ -50,20 +55,28 @@ if __name__ == '__main__':
 	import argparse
 	# file run: python image_class.py -I image_file.jpg -P True
 	parser = argparse.ArgumentParser(description="flag format given as: -I <image_file.jpg> -P <True/False>")
-	parser.add_argument('-I', '-image-file', help="file of images")
+	parser.add_argument('-I', '-image-directory', help="file of images")
 	parser.add_argument('-P', '-print-sentences', choices=("True", "False"), default="False", help="print sentences")
 	args = parser.parse_args()
 	
 	# get image file to classify
 	if args.I is None:
-		print("\nERROR: Include valid image file\n")
+		print("\nERROR: Include valid image directory\n")
 		exit()
 	else:
-		given_image_file = args.I
+		given_image_directory = args.I
 	
 	# argument to print details for the command prompt
 	to_print = args.P
 	to_print = True if args.P == 'True' else False # cast as true/false from input string
 
-	preProcessing(given_image_file, to_print)
+	# Reviewing and cleaning images (as needed)
+	preProcessing(given_image_directory, to_print)
+
+	# Morphological Classifer (TODO)
+	morphologicalClassifer(given_image_directory, to_print)
+
+	# Albedo Classifer (TODO)
+	albedoClassifer(given_image_directory, to_print)
+
 	print("\nran for for {0}\n".format(datetime.now() - start_time))
