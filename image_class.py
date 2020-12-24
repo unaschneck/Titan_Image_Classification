fd5f10395e9ca86002eb8328b5788c856d2fcfe5
cyschneck <22159116+cyschneck@users.noreply.github.com>
2020-12-24 00:59:58 -0700

    types of classifers with prelim details

--- image_class.py
+++ image_class.py
@@ -9,6 +9,41 @@ def preProcessing(image_file, to_print):
 	# run pre-processing on image file
 	if to_print: print("\ngiven image file: {0}".format(image_file))
 
+#### IMAGE TERRAIN CLASSIFERS: MORPHOLOGICAL and ALBEDO
+### TYPES: Craters, Plains, Labyrinth, Hummocky Terrain, Lakes, Dunes
+
+## MORPHOLOGICAL
+def morphologicalClassifer():
+	###############################################
+	## Based on the physical features
+	#
+	# Note: Preliminary features for classification
+	# Craters: Round
+	# Plains: Lacks topographic relief
+	# Labyrinth: Locally elevated
+	# Hummocky: Higher topographically than surrounding terrain
+	# Lakes: Typically has raised rims and steep sides
+	# Dunes: Long, narrow features with regular spacing
+	#
+	###############################################
+	pass
+
+## ALBEDO
+def albedoClassifer():
+	###############################################
+	## Based on the light or darkness of a region
+	#
+	# Note: Peleminary features for classification
+	# Craters: Low microwave Emissivity in ejecta blanket
+	# Plains: Dark in SAR, High Emissivity in RADAR
+	# Labyrinth: High emissivity
+	# Hummocky: Bright in SAR, Low Emissivity in RADAR, High Scatter in RADAR
+	# Lakes: Dark in SAR
+	# Dunes: Dark in SAR, High Emissivity in RADAR
+	#
+	###############################################
+	pass
+
 if __name__ == '__main__':
 	start_time = datetime.now()
 
